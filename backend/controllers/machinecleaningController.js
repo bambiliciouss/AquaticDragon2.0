6a5512596c534b1a017497f9bdb8c488241cae6c
bambiliciouss <ninapotmendoza917@gmail.com>
2024-02-21 01:01:24 +0800

    MachineCleaning Backend

--- backend/controllers/machinecleaningController.js
+++ backend/controllers/machinecleaningController.js
@@ -23,7 +23,7 @@ exports.createMachineCleaningRecord = async (req, res, next) => {
       );
     });
     const storeId = req.params.id;
-    const {  notes } = req.body;
+    const { notes } = req.body;
 
     const machinecleaning = await MachineCleaning.create({
       user: req.user.id,
@@ -44,5 +44,117 @@ exports.createMachineCleaningRecord = async (req, res, next) => {
     res
       .status(500)
       .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+exports.getStoreMachineCleaningDetails = async (req, res, next) => {
+  try {
+    const storeId = req.params.id;
+    if (!storeId) {
+      return next(
+        new ErrorHandler(`No record found: ${storeId}`)
+      );
+    }
+    const storeStoreMachineCleaning = await MachineCleaning.find({
+      storebranch: storeId,
+    });
+
+    // if (!storeStaff || storeStaff.length === 0) {
+
+    res.status(200).json({
+      success: true,
+      storeStoreMachineCleaning,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+exports.updateStoreMachineCleaning = async (req, res, next) => {
+  const newStoreData = {
+    notes: req.body.notes,
+  };
+
+  try {
+    if (req.body.cleaningImage && req.body.cleaningImage !== "") {
+      const machineCleaning = await MachineCleaning.findById(req.params.id);
+      const image_id = machineCleaning.cleaningImage.public_id;
+      const res = await cloudinary.uploader.destroy(image_id);
+      const result = await cloudinary.v2.uploader.upload(
+        req.body.cleaningImage,
+        {
+          folder: "machinecleaning",
+          width: 150,
+          crop: "scale",
+        },
+        (err, res) => {
+          console.log(err, res);
+        }
+      );
+      newStoreData.cleaningImage = {
+        public_id: result.public_id,
+        url: result.secure_url,
+      };
+    }
+    const machineCleaning = await MachineCleaning.findByIdAndUpdate(
+      req.params.id,
+      newStoreData,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(200).json({
+      success: true,
+      machineCleaning,
+      message: "pasok bhie",
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+exports.getSingleStoreMachineCleaningDetails = async (req, res, next) => {
+  try {
+    const storeStoreMachineCleaning = await MachineCleaning.findById(
+      req.params.id
+    );
+
+    // if (!storeStaff || storeStaff.length === 0) {
+
+    res.status(200).json({
+      success: true,
+      storeStoreMachineCleaning,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+exports.deleteSingleStoreMachineCleaning = async (req, res, next) => {
+  try {
+    const machineCleaning = await MachineCleaning.findByIdAndUpdate(
+      req.params.id,
+      { $set: { deleted: true } },
+      { new: true }
+    );
+
+    if (!machineCleaning) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Record not found" });
+    }
+
+    res
+      .status(200)
+      .json({ success: true, machineCleaning, message: "Record soft deleted" });
+  } catch (error) {
+    // Handle error, log, or send an appropriate response
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
--- backend/routes/machinecleaning.js
+++ backend/routes/machinecleaning.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../utils/multer");
 
 const {
   createMachineCleaningRecord,
+  getStoreMachineCleaningDetails,
+  updateStoreMachineCleaning,
+  getSingleStoreMachineCleaningDetails,
+  deleteSingleStoreMachineCleaning,
 } = require("../controllers/machinecleaningController");
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
@@ -10,7 +15,33 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 router.post(
   "/create/machinecleaning/record/:id",
   isAuthenticatedUser,
-  createMachineCleaningRecord
+  createMachineCleaningRecord,
+  upload.single("cleaningImage")
+);
+
+router.get(
+  "/store/machinecleaning/record/:id",
+  isAuthenticatedUser,
+  getStoreMachineCleaningDetails
+);
+
+router.put(
+  "/store/machinecleaning/record/:id",
+  isAuthenticatedUser,
+  upload.single("cleaningImage"),
+  updateStoreMachineCleaning
+);
+
+router.get(
+  "/machinecleaning/record/:id",
+  isAuthenticatedUser,
+  getSingleStoreMachineCleaningDetails
+);
+
+router.delete(
+  "/machinecleaning/record/:id",
+  isAuthenticatedUser,
+  deleteSingleStoreMachineCleaning
 );
 
 module.exports = router;
