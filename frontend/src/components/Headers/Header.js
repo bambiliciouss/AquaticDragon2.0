012a882241fbe0fa3ae964f2c4e8e0df87c84b84
bambiliciouss <ninapotmendoza917@gmail.com>
2024-04-08 15:26:09 +0800

    removed gallon card

--- frontend/src/components/Headers/Header.js
+++ frontend/src/components/Headers/Header.js
@@ -52,7 +52,7 @@ const Header = () => {
           <div className="header-body">
             {/* Card stats */}
             <Row>
-              <Col lg="6" xl="3">
+              <Col lg="6" xl="4">
                 <Card className="card-stats mb-4 mb-xl-0">
                   <CardBody>
                     <Row>
@@ -82,7 +82,7 @@ const Header = () => {
                   </CardBody>
                 </Card>
               </Col>
-              <Col lg="6" xl="3">
+              <Col lg="6" xl="4">
                 <Card className="card-stats mb-4 mb-xl-0">
                   <CardBody>
                     <Row>
@@ -114,7 +114,7 @@ const Header = () => {
                   </CardBody>
                 </Card>
               </Col>
-              <Col lg="6" xl="3">
+              <Col lg="6" xl="4">
                 <Card className="card-stats mb-4 mb-xl-0">
                   <CardBody>
                     <Row>
@@ -145,33 +145,7 @@ const Header = () => {
                   </CardBody>
                 </Card>
               </Col>
-              <Col lg="6" xl="3">
-                <Card className="card-stats mb-4 mb-xl-0">
-                  <CardBody>
-                    <Row>
-                      <div className="col">
-                        <CardTitle
-                          tag="h5"
-                          className="text-uppercase text-muted mb-0">
-                         Gallons
-                        </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">{gallons && gallons.length}</span>
-                      </div>
-                      <Col className="col-auto">
-                        <div className="icon icon-shape bg-info text-white rounded-circle shadow">
-                          <i className="fas fa-percent" />
-                        </div>
-                      </Col>
-                    </Row>
-                    {/* <p className="mt-3 mb-0 text-muted text-sm">
-                      <span className="text-success mr-2">
-                        <i className="fas fa-arrow-up" /> As of 
-                      </span>{" "}
-                      <span className="text-nowrap">{formattedDate}</span>
-                    </p> */}
-                  </CardBody>
-                </Card>
-              </Col>
+              
             </Row>
           </div>
         </Container>
