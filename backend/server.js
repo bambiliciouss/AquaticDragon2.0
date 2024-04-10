bfa36eb979e7b692950dbbd1b83152050264a8ed
bambiliciouss <ninapotmendoza917@gmail.com>
2024-04-10 14:18:24 +0800

    Refactor server.js to use Socket.io for real-time notifications

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,13 @@
 const app = require("./app");
+const http = require("http");
+
+const server = http.createServer(app);
+const socketIo = require("socket.io")(server, {
+  cors: {
+    origin: "*"
+  }
+});
+const io = socketIo;
 const connectDatabase = require("./config/database");
 const autoAgeUp = require("./config/updateAgeAuto");
 const notifyCleaning = require("./config/machineCleaningNotif");
@@ -7,6 +16,7 @@ const phychem = require("./config/phychemtestNotif");
 const notifyBusinessP = require("./config/businessPermit");
 const dotenv = require("dotenv");
 const cloudinary = require("cloudinary");
+const { getAdminIdFromBranch, notifyAdmin } = require("./utils/utils");
 
 dotenv.config({ path: "config/.env" });
 connectDatabase();
@@ -22,8 +32,41 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `server started on port:' ${process.env.PORT} in ${process.env.NODE_ENV} mode`
-  );
+let adminSockets = {};
+// Socket.io connection handling
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  // Handle client disconnection
+  socket.on('disconnect', () => {
+      console.log('Client disconnected');
+  });
+
+  // Handle 'notification' event from client
+  socket.on('newOrder', async (data) => {
+    const {message, branch} = data;
+    try{
+      const adminId = (await getAdminIdFromBranch(branch)).toString();
+      console.log("adminsockets: ", adminSockets)
+      console.log("admin: " ,adminSockets[adminId]);
+      console.log('Received notification in server:', message);
+      // Broadcast the received message to all connected clients
+      notifyAdmin(io, adminId, adminSockets, message);
+    }catch(error){
+      console.log(error);
+    }
+      
+  });
+  socket.on('login',(data)=>{
+    const {adminId}  = data;
+    adminSockets[adminId] = socket.id;
+    console.log(`Added admin: ${adminId} to adminSockets`)
+    console.log(adminSockets)
+  })
+});
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
