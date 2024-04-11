b4f0f033bf35f1bbf20bc1fbaafc484a1fe34e08
bambiliciouss <ninapotmendoza917@gmail.com>
2024-04-11 15:31:32 +0800

    Add socket connection and notification handling in server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const notifyBusinessP = require("./config/businessPermit");
 const dotenv = require("dotenv");
 const cloudinary = require("cloudinary");
 const { getAdminIdFromBranch, notifyAdmin } = require("./utils/utils");
-
+const { storeNotification, fetchNotification, markAsRead } = require('./controllers/notificationController')
 dotenv.config({ path: "config/.env" });
 connectDatabase();
 // autoAgeUp();
@@ -33,35 +33,70 @@ cloudinary.config({
 });
 
 let adminSockets = {};
+let orderItems = [];
+let orderProducts = [];
 // Socket.io connection handling
 io.on('connection', (socket) => {
   console.log('New client connected');
 
-  // Handle client disconnection
   socket.on('disconnect', () => {
-      console.log('Client disconnected');
+    for (let adminId in adminSockets) {
+      const socketIndex = adminSockets[adminId].indexOf(socket.id);
+      if (socketIndex !== -1) {
+        adminSockets[adminId].splice(socketIndex, 1);
+      }
+    }
   });
 
   // Handle 'notification' event from client
   socket.on('newOrder', async (data) => {
-    const {message, branch} = data;
-    try{
+    const { message, branch, title, order } = data;
+    try {
       const adminId = (await getAdminIdFromBranch(branch)).toString();
-      console.log("adminsockets: ", adminSockets)
-      console.log("admin: " ,adminSockets[adminId]);
+
       console.log('Received notification in server:', message);
-      // Broadcast the received message to all connected clients
-      notifyAdmin(io, adminId, adminSockets, message);
-    }catch(error){
+      console.log('order: ', order)
+
+      orderItems = order.orderItems;
+      orderProducts = order.orderProducts;
+      await storeNotification(message, title, orderItems, orderProducts, adminId, order._id);
+      const notifications = await fetchNotification(adminId);
+      if (adminSockets[adminId] && adminSockets[adminId].length > 0) {
+        // Emit the 'notification' event to all socket IDs for the admin ID
+        adminSockets[adminId].forEach(socketId => {
+          io.to(socketId).emit('notification', notifications);
+        });
+      }
+
+    } catch (error) {
       console.log(error);
     }
-      
+
   });
-  socket.on('login',(data)=>{
-    const {adminId}  = data;
-    adminSockets[adminId] = socket.id;
+  socket.on('login', async (data) => {
+    const { adminId } = data;
+    // If the adminId is not in adminSockets, add an empty array
+    if (!adminSockets[adminId]) {
+      adminSockets[adminId] = [];
+    }
+    // Only add the socket ID if it's not already in the array
+    if (!adminSockets[adminId].includes(socket.id)) {
+      adminSockets[adminId].push(socket.id);
+    }
     console.log(`Added admin: ${adminId} to adminSockets`)
     console.log(adminSockets)
+    const notifications = await fetchNotification(adminId);
+
+    // Emit the 'notification' event only to the socket that triggered the 'login' event
+    socket.emit('notification', notifications);
+  })
+
+  socket.on('readNotification', async (data) => {
+    const { notificationId, adminId } = data;
+    await markAsRead(notificationId);
+    const notifications = await fetchNotification(adminId);
+   
+    socket.emit('notification', notifications);
   })
 });
 
