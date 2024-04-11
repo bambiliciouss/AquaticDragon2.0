14f0e5bedca4a0c32606c0b873cc999e34e92270
bambiliciouss <ninapotmendoza917@gmail.com>
2024-04-11 15:31:59 +0800

    Refactor notifyAdmin function to include orderProducts in newOrder event

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,14 +1,25 @@
 const StoreBranch = require("../models/storeBranch")
-
+const TypeOfGallon = require("../models/typeofgallon")
 exports.getAdminIdFromBranch = async (branch) => {
     const adminId = await StoreBranch.findById(branch).select('user');
     return adminId.user;
 }
-
-exports.notifyAdmin = (io, adminId, adminSockets, message)=>{
+exports.getProductTypeName = async (productType) => {
+    const name = await TypeOfGallon.findById(productType).select('typeofGallon');
+    return name.typeofGallon;
+}
+exports.notifyAdmin = async (io, adminId, adminSockets, message, title, orderItems, orderProducts)=>{
     const socketId = adminSockets[adminId];
+
+    if (orderProducts && orderProducts.length > 0){
+        orderProducts = await Promise.all(orderProducts.map(async (product) => {
+            const name = await this.getProductTypeName(product.type);
+            return { ...product, type: name }
+        }))
+    }
+   console.log("orderProducts", orderProducts);
     if (socketId){
-        io.to(socketId).emit("newOrder", message);
+        io.to(socketId).emit("newOrder", {title: title,message: message, orderItems: orderItems, orderProducts: orderProducts});
     }
     
 }
