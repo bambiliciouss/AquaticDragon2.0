b749b7fcc26baab7769f3913d65ea7268c55540a
bambiliciouss <ninapotmendoza917@gmail.com>
2024-04-15 20:08:11 +0800

    removed duplicate declaration

--- frontend/src/components/gallon/RegisterGallon.js
+++ frontend/src/components/gallon/RegisterGallon.js
@@ -23,7 +23,6 @@ import { CREATE_GALLON_RESET } from "../../constants/gallonConstants";
 import { useForm } from "react-hook-form";
 import swal from "sweetalert";
 import { allTypesGallon } from "actions/typesgallonAction";
-import swal from 'sweetalert';
 const RegisterGallon = () => {
   const [gallon, setGallon] = useState({
     type: "",
