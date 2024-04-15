8b8eaf82df12b4c8552c926978d77692277cb4fd
bambiliciouss <ninapotmendoza917@gmail.com>
2024-04-15 20:07:58 +0800

    added cards to employee orderlist

--- frontend/src/components/employee/Orderlist.js
+++ frontend/src/components/employee/Orderlist.js
@@ -16,12 +16,15 @@ import QRCode from "react-qr-code";
 import { DELETE_GALLON_RESET } from "../../constants/gallonConstants";
 import swal from "sweetalert";
 import { allOrdersEmployee, clearErrors } from "../../actions/orderActions";
+import { allStoreSalesAction, getSalesWalkin, getSalesOrderByBranchEmployee, getEmployeeBranch } from "../../actions/adminAction";
+
 import Loader from "components/layout/Loader";
 import {
   Button,
   Card,
   CardHeader,
   CardBody,
+  CardTitle,
   NavItem,
   NavLink,
   Nav,
@@ -35,11 +38,24 @@ import {
 
 const EmployeeOrderList = () => {
   const dispatch = useDispatch();
-
+  const { user } = useSelector((state) => state.auth) // Get the user id
+  const { sales: walkinSales } = useSelector((state) => state.adminSalesWalkin); // Get the sales of all walk in orders
+  const {orders: orderSales} = useSelector((state)=> state.employeeOrderSales)
+  const { branches: branch } = useSelector((state) => state.employeeBranch) // Get employee branch
+  // const { sales } = useSelector((state) => state.adminStoreSales); // Get the sales of all stores
+  
   let navigate = useNavigate();
-
+  const [totalSales, setTotalSales] = useState(0)
   const { loading, error, orders } = useSelector((state) => state.allOrdersStaff);
-
+  // Function to calculate the total sales of the selected branch
+  const getTotalSales = (order, walkin) => {
+    if (order.length > 0 && walkin.length > 0) {
+      const totalSalesOrder = order.find((sale) => sale._id === branch.branches.storebranch).totalSales || 0;
+      const totalSalesWalkin = walkin.find((sale) => sale._id === branch.branches.storebranch).totalSales || 0;
+      setTotalSales(totalSalesOrder + totalSalesWalkin)
+      localStorage.setItem("totalSales", totalSalesOrder + totalSalesWalkin)
+    }
+  }
   useEffect(() => {
     dispatch(allOrdersEmployee());
     if (error) {
@@ -47,6 +63,39 @@ const EmployeeOrderList = () => {
     }
     console.log("Employee Orders", orders);
   }, [dispatch, error]);
+  // Universal UseEffect
+  useEffect(() => {
+    if (user.role === 'employee') {
+
+      // Action for sales state
+      // Gets the total sales of all stores
+      dispatch(allStoreSalesAction(user._id));
+
+      // Action for walkinSales state
+      // Gets the total sales of all walkin sales
+      dispatch(getSalesWalkin());
+
+      // Action for orders state
+      // Gets the total sales of all orders
+      dispatch(getSalesOrderByBranchEmployee(user._id));
+
+      //Get all admin branches
+      dispatch(getEmployeeBranch(user._id));
+      
+      if (error) {
+        dispatch(clearErrors())
+      }
+    }
+  }, [dispatch, error, user])
+
+
+  useEffect(() => {
+    if (branch && branch.branches && orderSales && walkinSales) {
+      getTotalSales(orderSales, walkinSales)
+      
+    }
+  }, [branch,orderSales, walkinSales])
+
 
   const setOrders = () => {
     let data = {
@@ -153,29 +202,115 @@ const EmployeeOrderList = () => {
         <AdminNavbar />
         <Header2 />
         <Container className="mt--7" fluid>
-          <Card className="bg-secondary shadow">
-            <CardHeader className="bg-white border-0">
-              <Row className="align-items-center">
-                <Col xs="8">
-                  <h3 className="mb-0">List of Order(s)</h3>
-                </Col>
-              </Row>
-            </CardHeader>
-            <CardBody style={{ overflowX: "auto" }}>
-              {loading ? (
-                <Loader />
-              ) : (
-                <MDBDataTable
-                  data={setOrders()}
-                  className="px-3"
-                  bordered
-                  hover
-                  noBottomColumns
-                  responsive
-                />
-              )}
-            </CardBody>
-          </Card>
+
+          <Row>
+            <Col className="mb-5 mb-xl-4" lg="6" xl="4">
+
+              <Card className="shadow card-stats mb-4 mb-xl-0">
+                <CardBody>
+                  <Row className="align-items-center">
+                    <div className="col">
+                      <CardTitle
+                        tag="h2"
+                        className="text-uppercase text-black mb-0  font-weight-bolder">
+                        Total Sales
+                      </CardTitle>
+
+                    </div>
+                    <Col className="col-auto">
+                      <CardTitle
+                        tag={branch && branch.branches ? "h1" : "h3"}
+                        className="text-uppercase text-primary mb-0 font-weight-bolder">
+                        {totalSales ? `₱${totalSales}` : localStorage.getItem("totalSales") ? `₱${localStorage.getItem("totalSales")}` : <span className="text-danger">Select a branch</span>}
+                      </CardTitle>
+
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+
+            </Col>
+            <Col className="mb-5 mb-xl-4" lg="6" xl="4">
+
+              <Card className="shadow card-stats mb-4 mb-xl-0">
+                <CardBody>
+                  <Row className="align-items-center">
+                    <div className="col">
+                      <CardTitle
+                        tag="h2"
+                        className="text-uppercase text-black mb-0  font-weight-bolder">
+                        Total Sales Ordering
+                      </CardTitle>
+
+                    </div>
+                    <Col className="col-auto">
+                      <CardTitle
+                        tag={orderSales && branch && branch.branches && orderSales.find((sale) => sale._id === branch.branches.storebranch) ? "h1" : "h3"}
+                        className="text-uppercase text-primary mb-0 font-weight-bolder">
+                        {orderSales && branch && branch.branches && orderSales.find((sale) => sale._id === branch.branches.storebranch) ? `₱${orderSales.find((sale) => sale._id === branch.branches.storebranch).totalSales}` : <span className="text-danger">Select a branch</span>}
+                      </CardTitle>
+
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+
+            </Col>
+            <Col className="mb-5 mb-xl-4" lg="6" xl="4">
+
+              <Card className="shadow card-stats mb-4 mb-xl-0">
+                <CardBody>
+                  <Row className="align-items-center">
+                    <div className="col">
+                      <CardTitle
+                        tag="h2"
+                        className="text-uppercase text-black mb-0  font-weight-bolder">
+                        Total Sales Walk In
+                      </CardTitle>
+
+                    </div>
+                    <Col className="col-auto">
+                      <CardTitle
+                        tag={walkinSales && branch && branch.branches && walkinSales.find((sale) => sale._id === branch.branches.storebranch) ? "h1" : "h3"}
+                        className="text-uppercase text-primary mb-0 font-weight-bolder">
+                        {walkinSales && branch && branch.branches && walkinSales.find((sale) => sale._id === branch.branches.storebranch) ? `₱${walkinSales.find((sale) => sale._id === branch.branches.storebranch).totalSales}` : <span className="text-danger">Select a branch</span>}
+                      </CardTitle>
+
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+
+            </Col>
+          </Row>
+
+          <Row>
+            <Col className="mb-5 mb-xl-0" xl="12">
+              <Card className="bg-secondary shadow">
+                <CardHeader className="bg-white border-0">
+                  <Row className="align-items-center">
+                    <Col xs="8">
+                      <h3 className="mb-0">List of Order(s)</h3>
+                    </Col>
+                  </Row>
+                </CardHeader>
+                <CardBody style={{ overflowX: "auto" }}>
+                  {loading ? (
+                    <Loader />
+                  ) : (
+                    <MDBDataTable
+                      data={setOrders()}
+                      className="px-3"
+                      bordered
+                      hover
+                      noBottomColumns
+                      responsive
+                    />
+                  )}
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
         </Container>
         <Container fluid>
           <AdminFooter />
