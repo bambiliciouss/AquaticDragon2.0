1f9ddf302cce5ade952234046c5c4d362c56cf0d
bambiliciouss <ninapotmendoza917@gmail.com>
2024-04-15 20:05:05 +0800

    Update CORS configuration in app.js to allow requests from localhost:3000

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,10 @@ app.use(cookieParser());
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 app.use("/api/v1", auth);
 app.use("/api/v1", gallon);
 app.use("/api/v1", storeBranch);
