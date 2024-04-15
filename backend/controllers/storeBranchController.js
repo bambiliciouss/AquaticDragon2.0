87450ec79536f37691870f72f643d8202713d17c
bambiliciouss <ninapotmendoza917@gmail.com>
2024-04-15 20:06:41 +0800

    Refactor storeBranchController.js to improve code readability and fix formatting issues and added sales order of employee branch

--- backend/controllers/storeBranchController.js
+++ backend/controllers/storeBranchController.js
@@ -247,7 +247,7 @@ exports.getAdminBranches = async (req, res, next) => {
 exports.getSalesOrderByBranch = async (req, res) => {
   try {
     const branches = await StoreBranch.find({ user: req.params.id });
-    const branchIds = branches.map((branch)=>branch._id);
+    const branchIds = branches.map((branch) => branch._id);
 
     const salesByBranch = await Order.aggregate([
       {
@@ -274,7 +274,7 @@ exports.getSalesOrderByBranch = async (req, res) => {
       },
       {
         $project: {
-          _id:1,
+          _id: 1,
           branch: '$store.branch',
           totalSales: 1,
         },
@@ -286,7 +286,49 @@ exports.getSalesOrderByBranch = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+exports.getSalesOrderByBranchEmployee = async (req, res) => {
+  try {
+    
+    const branch = await User.findById(req.params.id).select('storebranch');
+    
 
+    const salesByBranch = await Order.aggregate([
+      {
+        $match: {
+          'selectedStore.store': new mongoose.Types.ObjectId(branch.storebranch),
+        },
+      },
+      {
+        $group: {
+          _id: '$selectedStore.store',
+          totalSales: { $sum: '$totalPrice' },
+        },
+      },
+      {
+        $lookup: {
+          from: 'storebranches', // replace with the actual name of your store branches collection
+          localField: '_id',
+          foreignField: '_id',
+          as: 'store',
+        },
+      },
+      {
+        $unwind: '$store',
+      },
+      {
+        $project: {
+          _id: 1,
+          branch: '$store.branch',
+          totalSales: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json(salesByBranch);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 exports.getSalesByBranch = async (req, res) => {
   try {
     const branches = await StoreBranch.find({ user: req.params.id });
@@ -357,3 +399,151 @@ exports.getSalesByBranch = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+exports.getSalesOfCurrentBranch = async (req, res) => {
+  try {
+    //Id of selected branch
+    const branch = req.params.id;
+
+    // Filter value (today, past7days, monthly, yearly)
+    const filter = req.query.filter;
+    let dateFilter = {};
+    let groupBy = {};
+    let startDate = new Date();
+    let endDate = new Date();
+    // Create date filter based on filter value
+    const today = new Date();
+    if (filter === 'today') {
+      startDate = new Date();
+      startDate.setHours(0, 0, 0, 0); // set the time to the start of today
+
+      endDate = new Date();
+      endDate.setDate(endDate.getDate() + 1); // set the date to tomorrow
+      endDate.setHours(0, 0, 0, 0); // set the time to the start of tomorrow
+      dateFilter = { $gte: startDate, $lt: endDate };
+    } else if (filter === 'week') {
+      const today = new Date();
+
+      startDate = new Date();
+      startDate.setDate(today.getDate() - 6); // go back 6 days from today
+      startDate.setHours(0, 0, 0, 0); // set the time to the start of the day
+
+      endDate = new Date();
+      endDate.setHours(23, 59, 59, 999); // set the time to the end of the day
+
+      dateFilter = { $gte: startDate, $lt: endDate };
+    } else if (filter === 'month') {
+      dateFilter = { $gte: new Date(today.setMonth(0)), $lte: new Date(today.setMonth(11)) };
+    } else if (filter === 'year') {
+      dateFilter = { $gte: new Date(today.setFullYear(today.getFullYear() - 5)) };
+    }
+    //Group by date based on filter value
+    if (filter === 'today') {
+      groupBy = { $dateToString: { format: "%Y-%m-%d %H:00", date: "$createdAt" } };
+    } else if (filter === 'week') {
+      groupBy = { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } };
+    } else if (filter === 'month') {
+      groupBy = { $dateToString: { format: "%m", date: "$createdAt" } };
+    } else if (filter === 'year') {
+      groupBy = { $year: "$createdAt" };
+    }
+    const salesByBranch = await Order.aggregate([
+      {
+        $match: {
+          'selectedStore.store': new mongoose.Types.ObjectId(branch),
+          createdAt: dateFilter,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            'store': '$selectedStore.store',
+            'date': groupBy
+          },
+          totalSales: { $sum: '$totalPrice' },
+        },
+      },
+      {
+        $lookup: {
+          from: 'storebranches', // replace with the actual name of your store branches collection
+          localField: '_id.store',
+          foreignField: '_id',
+          as: 'store',
+        },
+      },
+      {
+        $unwind: '$store',
+      },
+      {
+        $lookup: {
+          from: 'othergallons', // replace with the actual name of your OtherGallon collection
+          localField: '_id',
+          foreignField: 'storebranch',
+          as: 'otherGallons',
+        },
+      },
+      {
+        $addFields: {
+          totalWalkInSales: {
+            $sum: {
+              $map: {
+                input: "$otherGallons",
+                as: "gallon",
+                in: {
+                  $multiply: ["$$gallon.price", "$$gallon.quantity"],
+                },
+              }
+            }
+          },
+        },
+      },
+      {
+        $addFields: {
+          totalSales: { $add: ["$totalSales", "$totalWalkInSales"] },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          branch: '$store.branch',
+          totalSales: 1,
+        },
+      },
+      {
+        $sort: { '_id.date': 1 }
+      }
+    ]);
+    if (filter === 'month') {
+      const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      salesByBranch.forEach(order => {
+        order._id.date = monthNames[parseInt(order._id.date) - 1];
+
+      });
+    }
+    res.status(200).json({
+      salesByBranch,
+      startDate: startDate.toLocaleDateString(),
+      endDate: endDate.toLocaleDateString()
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getEmployeeBranches = async (req, res) => {
+  try {
+    const userID = req.params.id
+    const branches = await User.findById(userID).select('storebranch')
+    if (!branches) {
+      return res.status(404).json({ success: false, message: "Branches not found" })
+    }
+    res.status(200).json({
+      success: true,
+      branches: branches
+    })
+
+
+  } catch (error) {
+    console.log(error)
+  }
+}
